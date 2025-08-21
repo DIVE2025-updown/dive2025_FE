503ef7462f85831ab74eb55464ea4f35728da2ad
Minju Jeon <odeto1310@gmail.com>
2025-08-21 23:53:05 +0900

    Feat: add Landing.vue

--- src/router/index.js
+++ src/router/index.js
@@ -4,40 +4,46 @@ import Map from '@/views/Map.vue';
 import Apply from '@/views/Apply.vue';
 import Stats from '@/views/Stats.vue';
 import Mypage from '@/views/Mypage.vue';
+import Landing from '@/views/Landing.vue';
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      component: Dashboard,
-      children: [
+    history: createWebHistory(import.meta.env.BASE_URL),
+    routes: [
         {
-          path: '',
-          redirect: 'map',
+            path: '/',
+            component: Dashboard,
+            children: [
+                {
+                    path: '',
+                    redirect: 'map',
+                },
+                {
+                    path: 'map',
+                    name: 'map',
+                    component: Map,
+                },
+                {
+                    path: 'apply',
+                    name: 'apply',
+                    component: Apply,
+                },
+                {
+                    path: 'stats',
+                    name: 'stats',
+                    component: Stats,
+                },
+                {
+                    path: 'mypage',
+                    name: 'mypage',
+                    component: Mypage,
+                },
+                {
+                    path: 'landing',
+                    name: 'landing',
+                    component: Landing,
+                },
+            ],
         },
-        {
-          path: 'map',
-          name: 'map',
-          component: Map,
-        },
-        {
-          path: 'apply',
-          name: 'apply',
-          component: Apply,
-        },
-        {
-          path: 'stats',
-          name: 'stats',
-          component: Stats,
-        },
-        {
-          path: 'mypage',
-          name: 'mypage',
-          component: Mypage,
-        },
-      ],
-    },
-  ],
+    ],
 });
 
 export default router;
